4bbf5366c76501bb86c9dd35ea930685ef17614d
Endorze <alexyurihallgren@hotmail.com>
2025-08-25 01:15:36 +0200

    fix: Autocomplete = off on inputfield

--- src/app/components/CreatePlayer/createPlayer.tsx
+++ src/app/components/CreatePlayer/createPlayer.tsx
@@ -9,7 +9,7 @@ const CreatePlayer = ({ setPlayer }: { setPlayer: (s: string) => void }): ReactE
         <div data-testid='createPlayer' className="flex flex-col gap-4 bg-[var(--color-bg-main)] text-white px-12 py-12 shadow-2xl shadow-gray-500 max-w-[400px]">
 
             <label htmlFor='name' className="tracking-wide text-2xl mb-3">Pick a name</label>
-            <input id='name' className="bg-white text-gray-900 px-2 placeholder:text-gray-500 outline-none" type="text" placeholder="Your name" value={currentInput} onChange={(e) => setCurrentInput(e.target.value)}
+            <input id='name' className="bg-white text-gray-900 px-2 placeholder:text-gray-500 outline-none" type="text" placeholder="Your name" autoComplete="off" value={currentInput} onChange={(e) => setCurrentInput(e.target.value)}
                 maxLength={15}></input>
                 
             <button className="bg-[var(--color-btn-bg)] disabled:text-gray-500 disabled:bg-[var(--color-link)] cursor-pointer disabled:cursor-not-allowed py-4" disabled={currentInput.length < 3} onClick={() => setPlayer(currentInput)}>Start game</button>
